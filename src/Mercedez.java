a037bcb6f49b6f217870fb10494feb5b0a709c46
D4_62972_Udhav <udhavpatil0806@gmail.com>
2024-04-17 18:30:52 +0530

    inheritance

--- src/Mercedez.java
+++ src/Mercedez.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Mercedez {
+public class Mercedez extends Vehicle{ // inheritance
+    private String name;
+    private int speed;
+    private float fuelCapacity;
+
+    public Mercedez(int modelNo, String Category, String name, int speed, float fuelCapacity){
+        super(modelNo, Category);
+        this.name = name;
+        this.speed = speed;
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public float getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(float fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public void display(){ // method overriding(runtime polymorphism)
+        super.display();
+        System.out.println(name+" "+speed+" "+fuelCapacity);
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Vehicle {
+public class Vehicle {
+    private int modelNo;
+    private String category;
+
+    public Vehicle(int modelNo, String category){
+        this.modelNo = modelNo;
+        this.category = category;
+    }
+
+    public void setModelNo(int modelNo) {
+        this.modelNo = modelNo;
+    }
+
+    public int getModelNo() {
+        return modelNo;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void display(){
+        System.out.println(modelNo+" "+category);
+    }
 }
